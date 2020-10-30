0e9dbc579593338493d69a695baf85ddfe1f9525
unknown <raniku>
2020-10-31 04:08:30 +0700

    Login API Laravel 8 With OAuth 2.0

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('signup', [App\Http\Controllers\AuthController::class, 'signup']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('helloworld', [App\Http\Controllers\AuthController::class, 'index']);
+    Route::post('logout', [App\Http\Controllers\AuthController::class, 'logout']);
 });
+
+
+
+
+    
+
